5d4812b3a9c1cd5a38c077303e013bcfdd354aff
1ArekKreft <arkadiuszkreft32@gmail.com>
2022-01-11 18:15:45 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -6,13 +7,30 @@ public class Main {
 
         /* zad.1 Napisz program który będzie iterował od 100 do 0,
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
-         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
+         * (jesli tą liczbą jest 0 to przez 2 ostatnie) MOJ INDEKS - 62849 */
 
+        for(int i=1;i<=100;i++)
+        {
+            if(i%9==0)
+            {
+                System.out.println(i);
+            }
+        }
+        System.out.println("");
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
-
+        Scanner scan = new Scanner(System.in);
+        int[] tab = new int[5];
+        for(int i=0;i<5;i++)
+        {
+            tab[i]=scan.nextInt();
+        }
+        for(int liczba : tab)
+        {
+            System.out.println(liczba+11);
+        }
     }
 }
